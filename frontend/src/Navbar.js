f94cf14bd00be0da88bfd5d93c0c7c76682b2740
vishnu2542 <7722vishnu@gmail.com>
2025-11-04 18:36:50 +0530

    added navbar

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">⚡ EVisionDrive</div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
